9147dc1b7f180396efc1fcea1bc0a60753487763
Bernardo A Mira García <68155136+bamg359@users.noreply.github.com>
2026-03-05 20:17:32 -0500

    US001: create user class

---
+++ src/main/java/com/cesde/cityscooterapp/domain/Customer.java
@@ -0,0 +1,84 @@
+package com.cesde.cityscooterapp.domain;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean status;
+
+    public Customer() {
+    }
+
+
+    public Customer(int id, String name, String lastName, String email, String password, boolean status) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
